37a174b9ab96e0f2b749177c04d9164159ca78dd
Aryan-Git-Hub <aryan.hmt1234@gmail.com>
2025-12-23 15:39:42 +0530

    installing required middlewares, i.e. body-parser, morgan, etd

--- server/app.js
+++ server/app.js
@@ -1,13 +1,19 @@
 import express from "express";
 import cors from "cors";
+import bodyParser from "body-parser";
+import morgan from "morgan";
 
 const app = express();
 
-app.use(cors());
-app.use(express.json());
+// Middlewares
+app.use(cors()); // Enable Cross-Origin Resource Sharing (CORS).
+app.use(express.json()); // Handles JSON data (like from an API call in React)
+app.use(bodyParser.urlencoded({ extended: true })); // Handles URL-encoded data (like from a standard HTML form)
+app.use(morgan('dev')); // It automatically logs every incoming HTTP request to your terminal console so you can see what is happening in real-time
+
 
 app.get("/", (req, res) => {
-    console.log("ES Module Backend Running");
+  console.log("ES Module Backend Running");
   res.send("ES Module Backend Running");
 });
 
