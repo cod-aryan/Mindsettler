0ac05a06f158180d72fbe20d07f81df6dd89527d
Aryan-Git-Hub <aryan.hmt1234@gmail.com>
2025-12-25 22:09:56 +0530

    minor

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,14 @@ import Home from './pages/Home';
 import PageNotFound from './pages/404';
 import AdminDashboard from './admin/Dashboard';
 import ScrollToTop from "./components/common/ScrollToTop";
+import Navbar from './components/common/Navbar';
 
 
 function App() {
   return (
     <BrowserRouter>
       <ScrollToTop />
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/admin" element={<AdminDashboard />} />
--- client/src/admin/dashboard.jsx
+++ client/src/admin/dashboard.jsx
@@ -1,9 +1,12 @@
 const AdminDashboard = () => {
   return (
+    <>
+    <div className="h-25" />
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
       <h1>Admin Page</h1>
       <p>Welcome to the admin panel.</p>
     </div>
+    </>
   );
 }
 
